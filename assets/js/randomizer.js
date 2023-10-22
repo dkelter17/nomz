5f904318d4fa0aaebfcca39a836688a6576c164e
Parker Moore <parkrmoore@gmail.com>
2023-10-22 10:25:09 -0700

    Fix indexOf

--- assets/js/randomizer.js
+++ assets/js/randomizer.js
@@ -36,7 +36,7 @@ function randomize(randomizerQuery) {
       seenWeekend = true
     } else if (candidate.day_of_week == "weekend") {
       i--
-    } else if (recipes.indexOf(candidate)) {
+    } else if (recipes.indexOf(candidate) >= 0) {
       i--
     } else {
       recipes.push(candidate)
