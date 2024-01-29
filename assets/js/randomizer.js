05cbfe724b14e28bc3c548789bb29e15027055b9
Parker Moore <parkrmoore@gmail.com>
2024-01-29 00:10:33 -0800

    Limit randomizer to dinners and stop using innerHTML

--- assets/js/randomizer.js
+++ assets/js/randomizer.js
@@ -31,7 +31,9 @@ function randomize(randomizerQuery) {
   for (var i = 0; i < randomizerQuery.numResults; i++) {  // Generate random return values
     const randomIndex = Math.floor(Math.random() * document.recipesRandomizer.recipes.length)
     const candidate = document.recipesRandomizer.recipes[randomIndex]
-    if (candidate.day_of_week == "weekend" && weekendRecipesSeen < randomizerQuery.maxWeekendRecipes){
+    if (candidate.category != "dinner") {
+      i--
+    } else if (candidate.day_of_week == "weekend" && weekendRecipesSeen < randomizerQuery.maxWeekendRecipes){
       recipes.push(candidate)
       seenWeekend = true
     } else if (candidate.day_of_week == "weekend") {
@@ -49,29 +51,47 @@ function displayRecipeResults(recipes) {
   var randomizerResults = document.getElementById('randomizer-results');
 
   if (recipes && recipes.length > 0) { // Are there any results?
-    var appendString = '<ol>';
+    const resultsList = document.createElement('ol')
+    for(const childNode of randomizerResults.childNodes) {
+      randomizerResults.removeChild(childNode)
+    }
     console.error(recipes)
 
     for (var i = 0; i < recipes.length; i++) {  // Iterate over the results
-      var item = recipes[i]
-      var source = ''
-      var title = '<h3>' + item.title + '</h3>'
+      const item = recipes[i]
+      const recipeListItem = document.createElement('li')
+
+      const titleH3 = document.createElement('h3')
       if (item.url) {
-        title = '<a href="' + item.url + '">' + title + '</a>'
+        const titleLink = document.createElement('a')
+        titleLink.href = item.url
+        titleLink.innerText = item.title
+        titleH3.appendChild(titleLink)
+        recipeListItem.appendChild(titleH3)
       } else {
-        source = '<p class="no-bottom-margin">Source: ' + item.source + '</p>'
+        titleH3.innerText = item.title
+        const source = document.createElement('p')
+        source.classList.add('no-bottom-margin')
+        source.innerText = `Source: ${item.source}`
+        recipeListItem.appendChild(titleH3)
+        recipeListItem.appendChild(source)
       }
-      var tags = item.tags || []
-      appendString += '<li>' + title + source + '</li>'
+
+      resultsList.appendChild(recipeListItem)
+      const tags = item.tags || []
       if (tags.length > 0) {
         const tagsWithHash = tags.map((tag) => `#${tag}`)
-        appendString += '<p>Tags: ' + tagsWithHash.join(" ") + '</p>'
+        const tagsListEl = document.createElement('p')
+        tagsListEl.innerText = `Tags: ${tagsWithHash.join(' ')}`
+        resultsList.appendChild(tagsListEl)
       }
     }
 
-    randomizerResults.innerHTML = appendString + "</ol>";
+    randomizerResults.appendChild(resultsList)
   } else {
-    randomizerResults.innerHTML = '<li>No results found</li>';
+    const noResultsFound = document.createElement('li')
+    noResultsFound.innerText = 'No results found'
+    randomizerResults.appendChild(noResultsFound)
   }
 }
 
